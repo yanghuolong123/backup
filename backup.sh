9186ee53f2c24b3aa8b62b514054e0e051cdcf7a
杨活龙 <yanghuolong@zhisland.com>
2018-04-12 10:21:53 +0800

    backup

--- backup.sh
+++ backup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #50 10 * * * /var/work/backup/backupmysql.sh
-cd /var/work/backup
+cd /var/work/backup/miaopost/sqldata
 dir=`date "+%Y-%m"`
 mkdir -p $dir
 cd $dir
 subfix=`date "+%Y-%m-%d_%H%M%S"`
 mysqldump -u root -pyanghuolong123 zd_miaopost --skip-lock-tables > miaopost_"$subfix".sql
+cp -r /var/work/web/goweb/miaopost/frontend/uploads /var/work/backup/miaopost
 
 git add .
-git commit -m "sql backup"
+git commit -m "backup"
 git push origin master:master
