ca418414af49df41528b6ca0aa2e5c2adc2e77cb
root <root@localhost.localdomain>
2018-05-28 10:54:51 +0800

    backup

--- backup-miaopost.sh
+++ backup-miaopost.sh
@@ -8,12 +8,6 @@ subfix=`date "+%Y-%m-%d_%H%M%S"`
 mysqldump -u root -pyanghuolong123 zd_miaopost --skip-lock-tables > miaopost_"$subfix".sql
 cp -r /var/work/web/goweb/miaopost/frontend/uploads /var/work/backup/miaopost
 
-cd /var/work/backup/yhlbbs/sqldata
-dir=`date "+%Y-%m"`
-mkdir -p $dir
-cd $dir
-subfix=`date "+%Y-%m-%d_%H%M%S"`
-mysqldump -u root -pyanghuolong123 yhlbbs --skip-lock-tables > yhlbbs_"$subfix".sql
 
 cd /var/work/backup
 
--- backup-yhlbbs.sh
+++ backup-yhlbbs.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 #50 10 * * * /var/work/backup/backupmysql.sh
-cd /var/work/backup/miaopost/sqldata
-dir=`date "+%Y-%m"`
-mkdir -p $dir
-cd $dir
-subfix=`date "+%Y-%m-%d_%H%M%S"`
-mysqldump -u root -pyanghuolong123 zd_miaopost --skip-lock-tables > miaopost_"$subfix".sql
-cp -r /var/work/web/goweb/miaopost/frontend/uploads /var/work/backup/miaopost
 
 cd /var/work/backup/yhlbbs/sqldata
 dir=`date "+%Y-%m"`
